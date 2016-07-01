16f8da8d4ef979657b273746e152229d04cfccea
tabish.khan <tabish.khan@tothenew.com>
2016-07-01 11:33:58 +0530

    Added initial structures and some sample APIs

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+
+var tracker = require('pivotaltracker');
+var user = '***********';
+var password = '******';
+var a = "";
+
+var client = tracker.Client();
+
+tracker.getToken(user, password, function(error, token) {
+  console.log(token, error);
+  a= token;
+  client.useToken(a);
+});
+
+app.get('/token', function (req, res) {
+   res.send( a);
+});
+
+app.get('/', function (req, res) {
+  client.accounts.all(function(error, accounts) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+
+      res.send(accounts);
+    }
+  });
+});
+
+app.get('/epics', function (req, res) {
+  client.projects.all(function(error, projects) {
+
+    client.project(projects[0].id).epics.all(function(error, stories) {
+
+     res.send(stories)
+    });
+  });
+});
+
+app.get('/project', function (req, res) {
+  client.projects.all(function(error, projects) {
+    res.send(projects)
+  });
+});
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3000!');
+});
