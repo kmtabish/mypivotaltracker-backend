e1a797d883b9ff3e5bcc80df83fbe6309c7e0f9d
Sakshi Tyagi <tyagi.sakshi09@gmail.com>
2016-07-07 11:50:54 +0530

    Corrected port number in console logs and updated package.json

--- app.js
+++ app.js
@@ -47,5 +47,5 @@ app.get('/project', function (req, res) {
 });
 
 app.listen(3001, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3001!');
 });
