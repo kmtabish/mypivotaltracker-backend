506068a78640195b53710b950f32c220913b8942
Sakshi Tyagi <tyagi.sakshi09@gmail.com>
2016-07-20 13:27:29 +0530

    Added checks for response and set status code

--- controllers/user.js
+++ controllers/user.js
@@ -5,10 +5,15 @@ module.exports = {
     console.log("Body Response--user", req.body);
     if (req.body.username && req.body.password) {
       tracker.getToken(req.body.username, req.body.password, function (error, token) {
-        //    console.log
+        if(error)
+          res.status(404).send('No user found');
+        else
+          if(token){
+            res.status(200).res.send({token : token});
+          }
       });
     }else{
-      res.send({statusCode: "500"})
+      res.status(500).send('Internal server error');
     }
   },
   get: function () {
