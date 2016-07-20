dbcef078978718fd9bcbef076dbf7a87993c0656
tabish.khan <tabish.khan@tothenew.com>
2016-07-20 15:37:06 +0530

    Added user token authentication and updated the login module

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
-app.use(bodyParser());
+
+//app.use(bodyParser());
 
 var projectController = require('./controllers/project');
 var epicController = require('./controllers/epic');
 var userController = require('./controllers/user');
 
 app.post('/login',userController.login);
+app.post('/verifyToken',userController.verifyToken);
 
 /*app.get('/epics', function (req, res) {
   client.projects.all(function(error, projects) {
@@ -31,6 +36,6 @@ app.get('/project', function (req, res) {
   });
 });*/
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
--- controllers/user.js
+++ controllers/user.js
@@ -2,16 +2,29 @@ var tracker = require('pivotaltracker');
 
 module.exports = {
   login: function (req, res) {
-    console.log("Body Response--user", req.body);
     if (req.body.username && req.body.password) {
       tracker.getToken(req.body.username, req.body.password, function (error, token) {
-        //    console.log
+        res.send({userToken: token})
       });
     }else{
-      res.send({statusCode: "500"})
+      res.status(500).send('internal server error');
     }
   },
-  get: function () {
+  verifyToken: function (req, res) {
+    if(req.body.userToken){
+      var client = tracker.Client();
+      client.useToken(req.body.userToken);
+      client.projects.all(function(error, projects) {
+        if(error){
+          res.status(500).send('Invalid User Token');
+        }else{
+          res.status(200).send('Valid User Token');
+        }
+      });
 
+    }else{
+      res.status(500).send('internal server error');
+
+    }
   }
 };
